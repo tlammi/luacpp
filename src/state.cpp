8bb25dec5484cc28f2ddc29963235d69834fe18d
tlammi <tangojulietlima@gmail.com>
2022-07-31 10:44:21 +0300

    Refactoring

--- src/state.cpp
+++ src/state.cpp
@@ -28,6 +28,15 @@ void create_libtable(lua_State* st, int field_count) {
   lua_createtable(st, 0, field_count);
 }
 
+template <class Iterable>
+void publish_functions(lua_State* st, const Iterable& container) {
+  for (const auto& [name, func] : container) {
+    lua_pushlightuserdata(st, func.get());
+    lua_pushcclosure(st, callback_lua, 1);
+    lua_setfield(st, -3, name.c_str());
+    lua_pop(st, 1);  // pop upvalue
+  }
+}
 }  // namespace
 
 State::State(flags::StateFlags f) : m_handle{cast(luaL_newstate())} {
@@ -71,18 +80,19 @@ void State::add_library(Library&& lib) {
     // luaL_checkversion(st);
     // lua_createtable(st, 0, lib.m_functions.size());
     //  luaL_setfuncs(st, funcs, 0);
-    for (const auto& [name, impl] : lib.m_functions) {
-      lua_pushlightuserdata(st, impl.get());
-      lua_pushcclosure(st, callback_lua, 1);
-      lua_setfield(st, -3, name.c_str());
-      lua_pop(st, 1);  // pop upvalue
-    }
-    // implement this
-    // lua_pushcfunction(L, openf);
-    // lua_pushstring(st, modname);   /* argument to open function */
-    // lua_call(st, 1, 1);            /* call 'openf' to open module */
-    // lua_pushvalue(st, -1);         /* make copy of module (call result) */
-    // lua_setfield(st, -3, modname); /* LOADED[modname] = module */
+    publish_functions(st, lib.m_functions);
+    // for (const auto& [name, impl] : lib.m_functions) {
+    //   lua_pushlightuserdata(st, impl.get());
+    //   lua_pushcclosure(st, callback_lua, 1);
+    //   lua_setfield(st, -3, name.c_str());
+    //   lua_pop(st, 1);  // pop upvalue
+    // }
+    //  implement this
+    //  lua_pushcfunction(L, openf);
+    //  lua_pushstring(st, modname);   /* argument to open function */
+    //  lua_call(st, 1, 1);            /* call 'openf' to open module */
+    //  lua_pushvalue(st, -1);         /* make copy of module (call result) */
+    //  lua_setfield(st, -3, modname); /* LOADED[modname] = module */
   }
   lua_remove(st, -2);         /* remove LOADED table */
   lua_pushvalue(st, -1);      /* copy of module */
