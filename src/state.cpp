d9cb991b50fc6a26a01acfc5d475d7292295ce58
tlammi <tangojulietlima@gmail.com>
2022-07-31 10:20:37 +0300

    Actually register callbacks from interface

--- src/state.cpp
+++ src/state.cpp
@@ -10,20 +10,17 @@ namespace luacpp {
 
 namespace {
 
-int bar(lua_State* s) {
-  std::cerr << "bar called!!!!\n";
-  lua_pushstring(s, "foo");
-  lua_gettable(s, LUA_REGISTRYINDEX);
-  detail::Func* f = (detail::Func*)lua_touserdata(s, -1);
-  (*f)(cast(s));
+/**
+ * Callback called for all lua functions
+ *
+ * The actual function implementation is stored in upvalues and this callback
+ * then calls the actual implementation.
+ */
+int callback_lua(lua_State* s) {
+  detail::Func* ptr =
+      static_cast<detail::Func*>(lua_touserdata(s, lua_upvalueindex(1)));
+  (*ptr)(cast(s));
   return 0;
-}
-
-constexpr luaL_Reg funcs[] = {{"foo", bar}, {nullptr, nullptr}};
-
-int open_nop(lua_State* s) {
-  luaL_newlib(s, funcs);
-  return 1;
 }
 
 }  // namespace
@@ -57,10 +54,33 @@ void State::dostring(const char* str) {
 }
 
 void State::add_library(Library&& lib) {
-  lua_pushstring(cast(m_handle), "foo");
-  lua_pushlightuserdata(cast(m_handle), lib.m_functions.at(0).second.get());
-  lua_settable(cast(m_handle), LUA_REGISTRYINDEX);
-  luaL_requiref(cast(m_handle), lib.m_name.c_str(), open_nop, 1);
+  lua_State* st = cast(m_handle);
+  const char* modname = lib.m_name.c_str();
+
+  luaL_getsubtable(st, LUA_REGISTRYINDEX, LUA_LOADED_TABLE);
+  lua_getfield(st, -1, modname); /* LOADED[modname] */
+  if (!lua_toboolean(st, -1)) {  /* package not already loaded? */
+    lua_pop(st, 1);              /* remove field */
+    // luaL_newlib(st, funcs);
+    luaL_checkversion(st);
+    lua_createtable(st, 0, lib.m_functions.size());
+    // luaL_setfuncs(st, funcs, 0);
+    for (const auto& [name, impl] : lib.m_functions) {
+      lua_pushlightuserdata(st, impl.get());
+      lua_pushcclosure(st, callback_lua, 1);
+      lua_setfield(st, -3, name.c_str());
+      lua_pop(st, 1);  // pop upvalue
+    }
+    // implement this
+    // lua_pushcfunction(L, openf);
+    // lua_pushstring(st, modname);   /* argument to open function */
+    // lua_call(st, 1, 1);            /* call 'openf' to open module */
+    // lua_pushvalue(st, -1);         /* make copy of module (call result) */
+    // lua_setfield(st, -3, modname); /* LOADED[modname] = module */
+  }
+  lua_remove(st, -2);         /* remove LOADED table */
+  lua_pushvalue(st, -1);      /* copy of module */
+  lua_setglobal(st, modname); /* _G[modname] = module */
 }
 
 }  // namespace luacpp
