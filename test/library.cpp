54d4cb45313276636c729b1e4d564f4305be5957
tlammi <tangojulietlima@gmail.com>
2022-07-31 11:07:26 +0300

    Refactoring and new tests

--- test/library.cpp
+++ test/library.cpp
@@ -4,18 +4,53 @@
 
 #include "luacpp/state.hpp"
 
-TEST(Ctor, Init) { luacpp::Library l{"mylib"}; }
+TEST(Init, Ctor) { luacpp::Library l{"mylib"}; }
 
-void foo() { std::cerr << "foo called :o\n"; }
-
-TEST(Ctor, AddLib) {
+TEST(Init, AddLib) {
   luacpp::State s{};
   luacpp::Library mylib{"mylib"};
-  bool called = false;
-  mylib.add_function("foo", [&]() { called = true; });
-  mylib.add_function("bar", &foo);
   s.add_library(std::move(mylib));
+}
+
+bool g_foo_called = false;
+void foo() { g_foo_called = true; }
+
+TEST(Func, NoArgsVoid) {
+  g_foo_called = false;
+  luacpp::State s{};
+  luacpp::Library mylib{"mylib"};
+  mylib.add_function("foo", &foo);
+  s.add_library(std::move(mylib));
+  ASSERT_FALSE(g_foo_called);
+  s.dostring("mylib:foo()");
+  ASSERT_TRUE(g_foo_called);
+}
+
+TEST(Lambda, NoArgVoid) {
+  luacpp::State s{};
+  luacpp::Library lib{"lib"};
+  bool called = false;
+  lib.add_function("f", [&]() { called = true; });
   ASSERT_FALSE(called);
-  s.dostring("mylib:foo(); mylib:bar()");
+  s.add_library(std::move(lib));
+  s.dostring("lib:f()");
   ASSERT_TRUE(called);
+}
+
+TEST(MultipleFuncs, NoArgsVoid) {
+  luacpp::State s{};
+  luacpp::Library lib{"lib"};
+  g_foo_called = false;
+  lib.add_function("f", &foo);
+  bool lambda_called = false;
+  lib.add_function("lambda", [&]() { lambda_called = true; });
+  s.add_library(std::move(lib));
+
+  ASSERT_FALSE(g_foo_called);
+  ASSERT_FALSE(lambda_called);
+  s.dostring("lib:f()");
+  ASSERT_TRUE(g_foo_called);
+  ASSERT_FALSE(lambda_called);
+  s.dostring("lib:lambda()");
+  ASSERT_TRUE(lambda_called);
 }
