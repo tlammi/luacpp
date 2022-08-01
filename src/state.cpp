5cc13940a4eb83874bf787130b5249d1b158af55
tlammi <tangojulietlima@gmail.com>
2022-08-01 11:05:28 +0300

    Undo. Caused a dangling reference

--- src/state.cpp
+++ src/state.cpp
@@ -76,10 +76,10 @@ void State::add_library(Library&& lib) {
     lua_pop(st, 1);              /* remove field */
     create_libtable(st, lib.m_functions.size());
     publish_functions(st, lib.m_functions);
-    m_libs.push_back(std::move(lib));
   }
   lua_setglobal(st, modname); /* _G[modname] = module */
   lua_pop(st, 1);
+  m_libs.push_back(std::move(lib));
 }
 
 }  // namespace luacpp
